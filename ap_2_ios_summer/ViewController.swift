4c7f24acbc115494107c14389048d79e4d84bde2
rei-yoshi <rei.yosida.5a@stu.hosei.ac.jp>
2019-07-15 13:54:39 +0900

    d

---
+++ ap_2_ios_summer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ap_2_ios_summer
+//
+//  Created by 吉田れい on 2019/07/15.
+//  Copyright © 2019 吉田れい. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
